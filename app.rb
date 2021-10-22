1a5150eb4088be0edb76a980f7a74d7c7125a936
AlexRXM <alexander.rxm@gmail.com>
2021-10-22 22:22:27 +0400

    save comment to db

--- app.rb
+++ app.rb
@@ -59,12 +59,29 @@ get '/details/:post_id' do
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
 	@row = results[0]
 
+	@comments = db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
 	erb :details
+	
 end
 
 post '/details/:post_id' do
 
 	post_id = params[:post_id]
 	content = params[:content]
+	@db.execute 'insert into Comments 
+	(
+		content, 
+		created_date,
+		post_id
+	)
+	 values 
+	 (
+	 		?, 
+	 		datetime(),
+	 		?
+	 )', [content, post_id]
+
+	 redirect to('/details/' + post_id)
 
 end
